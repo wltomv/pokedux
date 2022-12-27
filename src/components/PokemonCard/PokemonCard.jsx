1a9ca1d40b965dd25f6667bc3cae70222b8d6e3c
Walter Mach <owaldovelasz@gmail.com>
2022-12-26 21:51:23 -0600

    Remove comments in PokemonCard Component

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -7,15 +7,9 @@ import { IconContext } from "react-icons";
 const PokemonCard = () => {
     return (
         <div className="card">
-            {/* <IconContext.Provider value={{ color: "white", size: "20px" }}> */}
             <button onClick={() => alert("Settings page")} className="star">
                 <BsFillStarFill />
             </button>
-            {/* <a href="https://www.google.com">
-                <FaGoogle />
-            </a> */}
-            {/* </IconContext.Provider> */}
-
             <div className="contentBox">
                 <img
                     src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"
