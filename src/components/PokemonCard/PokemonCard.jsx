b56a3683c3aa66abe825a659b6bd14e20cd0c74f
Walter Mach <owaldovelasz@gmail.com>
2022-12-26 21:47:26 -0600

    Add icon for favorites in the card component

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./PokemonCard.css";
+import { BsFillStarFill } from "react-icons/bs";
+import { FaGoogle } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const PokemonCard = () => {
     return (
         <div className="card">
+            {/* <IconContext.Provider value={{ color: "white", size: "20px" }}> */}
+            <button onClick={() => alert("Settings page")} className="star">
+                <BsFillStarFill />
+            </button>
+            {/* <a href="https://www.google.com">
+                <FaGoogle />
+            </a> */}
+            {/* </IconContext.Provider> */}
+
             <div className="contentBox">
                 <img
                     src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"
