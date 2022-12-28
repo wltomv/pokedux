7f3c3f2b4c0a40f3cd6c1d14d9f4fcc83c41f283
Walter Mach <owaldovelasz@gmail.com>
2022-12-28 11:54:36 -0600

    Add api request configurations

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getPokemons = () => {
+    return axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then((res) => res.data.results)
+        .catch((err) => console.log(err));
+};
